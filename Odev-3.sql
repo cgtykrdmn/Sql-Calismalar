9ed6426b3b5251e7899664a38f34607f24ce25d6
Cagatay Karaduman <karadumancagatay@gmail.com>
2023-05-21 17:13:56 +0300

    Sql-Odev-3

---
+++ Odev-3.sql
@@ -0,0 +1,10 @@
+SELECT country FROM country
+WHERE country LIKE 'A%a'
+
+WHERE country LIKE '_____%n'
+
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t%'
+
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99
